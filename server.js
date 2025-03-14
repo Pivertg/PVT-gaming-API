6ec9e9ab5a070480515acf2af05c47219de3b021
Pivertg <gamingpivert@gmail.com>
2025-03-14 20:47:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,4 +25,15 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Quelque chose a mal tourné!' });
 });
+const mongoose = require('mongoose');
+
+// Connexion à la base de données MongoDB
+mongoose.connect('mongodb://localhost:27017/monProjet', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connecté à MongoDB');
+}).catch((err) => {
+  console.log('Erreur de connexion à MongoDB:', err);
+});
 
