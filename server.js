b012bed30f53d240af48d1ee78ee28e2ebbedd0d
Pivertg <gamingpivert@gmail.com>
2025-03-21 16:51:02 +0100

    Correction CORS pour connexion API avec le site

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://pivertg.github.io"
+}));
 app.use(express.json());
 
 // Route principale
