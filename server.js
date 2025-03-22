e64b75c90b07dd2219c6bbab3ac5e34fc4d24dac
Pivertg <gamingpivert@gmail.com>
2025-03-22 15:28:17 +0100

    Mon message de commit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("Bienvenue sur l'API PVT Gaming !");
+});
+
 app.use(cors({
     origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE"],
